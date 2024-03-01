89e53550ea2a7413c7a74c81f49f876cde653bcb
KingLu <lushize@gmail.com>
2024-03-01 16:24:28 +0800

    Initial commit

---
+++ src/test/java/com/abchina/taihang/RenzhengZhongxin/RenzhengZhongxinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abchina.taihang.RenzhengZhongxin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenzhengZhongxinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
